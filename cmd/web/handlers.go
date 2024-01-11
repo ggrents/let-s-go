687aee2fea711cea16df5397726522aff4078e53
ggrents <chef_grents@mail.ru>
2024-01-12 00:48:39 +0600

    201 pages done

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,54 +1,92 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
-	"log"
+	"gggrents/golangproject/pkg/models"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 )
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-	http.NotFound(w, r)
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	// Because Pat matches the "/" path exactly, we can now remove the manual check
+	// Because Pat matches the "/" path exactly, we can now remove the manual check
+	// of r.URL.Path != "/" from this handler.
+	s, err := app.snippets.Latest()
+	if err != nil {
+	app.serverError(w, err)
 	return
 	}
-	// Initialize a slice containing the paths to the two files. Note that the
-	// home.page.tmpl file must be the *first* file in the slice.
-	files := []string{
-	"./ui/html/home.page.tmpl",
-	"./ui/html/base.layout.tmpl",
-	"./ui/html/footer.partial.tmpl",
-	
+	app.render(w, r, "home.page.tmpl", &templateData{
+	Snippets: s,
+	})
 	}
-	// Use the template.ParseFiles() function to read the files and store the
-	// templates in a template set. Notice that we can pass the slice of file paths
-	// as a variadic parameter?
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-	log.Println(err.Error())
-	http.Error(w, "Internal Server Error", 500)
+	func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	// Pat doesn't strip the colon from the named capture key, so we need to
+	// get the value of ":id" from the query string instead of "id".
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
+	if err != nil || id < 1 {
+	app.notFound(w)
 	return
 	}
-	err = ts.Execute(w, nil)
+	s, err := app.snippets.Get(id)
 	if err != nil {
-	log.Println(err.Error())
-	http.Error(w, "Internal Server Error", 500)
+	if errors.Is(err, models.ErrNoRecord) {
+	app.notFound(w)
+	} else {
+	app.serverError(w, err)
 	}
+	return
 	}
-	
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-id, err := strconv.Atoi(r.URL.Query().Get("id"))
-if err != nil || id < 1 {
-http.NotFound(w, r)
-return
-}
-fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-if r.Method != http.MethodPost {
-w.Header().Set("Allow", http.MethodPost)
-http.Error(w, "Method Not Allowed", 405)
-return
-}
-w.Write([]byte("Create a new snippet..."))
-}
+	app.render(w, r, "show.page.tmpl", &templateData{
+	Snippet: s,
+	})
+	}
+	// Add a new createSnippetForm handler, which for now returns a placeholder response.
+	func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+		app.render(w, r, "create.page.tmpl", nil)
+		}
+		
+		func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+			err := r.ParseForm()
+			if err != nil {
+			app.clientError(w, http.StatusBadRequest)
+			return
+			}
+			title := r.PostForm.Get("title")
+			content := r.PostForm.Get("content")
+			expires := r.PostForm.Get("expires")
+			errors := make(map[string]string)
+			if strings.TrimSpace(title) == "" {
+			errors["title"] = "This field cannot be blank"
+			} else if utf8.RuneCountInString(title) > 100 {
+			errors["title"] = "This field is too long (maximum is 100 characters)"
+			}
+			if strings.TrimSpace(content) == "" {
+			errors["content"] = "This field cannot be blank"
+			}
+			if strings.TrimSpace(expires) == "" {
+			errors["expires"] = "This field cannot be blank"
+			} else if expires != "365" && expires != "7" && expires != "1" {
+			errors["expires"] = "This field is invalid"
+			}
+			// If there are any validation errors, re-display the create.page.tmpl
+			// template passing in the validation errors and previously submitted
+			// r.PostForm data.
+			if len(errors) > 0 {
+			app.render(w, r, "create.page.tmpl", &templateData{
+			FormErrors: errors,
+			FormData: r.PostForm,
+			})
+			return
+			}
+			id, err := app.snippets.Insert(title, content, expires)
+			if err != nil {
+			app.serverError(w, err)
+			return
+			}
+			http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
+			}
+			
+			
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,33 +1,62 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"gggrents/golangproject/pkg/models/mysql"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
-func main() {
-	// Define a new command-line flag with the name 'addr', a default value of ":4000"
-	// and some short help text explaining what the flag controls. The value of the
-	// flag will be stored in the addr variable at runtime.
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+	snippets *mysql.SnippetModel
+	templateCache map[string]*template.Template
+	}
+	func main() {
+	dsn := flag.String("dsn", "root:my-secret-pw@/snippetbox?parseTime=true", "MySQL data source name")
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	// Importantly, we use the flag.Parse() function to parse the command-line flag.
-	// This reads in the command-line flag value and assigns it to the addr
-	// variable. You need to call this *before* you use the addr variable
-	// otherwise it will always contain the default value of ":4000". If any errors are
-	// encountered during parsing the application will be terminated.
 	flag.Parse()
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	// The value returned from the flag.String() function is a pointer to the flag
-	// value, not the value itself. So we need to dereference the pointer (i.e.
-	// prefix it with the * symbol) before using it. Note that we're using the
-	// log.Printf() function to interpolate the address with the log message.
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	db, err := openDB(*dsn)
+	if err != nil {
+	errorLog.Fatal(err)
+	}
+	defer db.Close()
+	// Initialize a new template cache...
+	templateCache, err := newTemplateCache("./ui/html/")
+	if err != nil {
+	errorLog.Fatal(err)
+	}
+	// And add it to the application dependencies.
+	app := &application{
+	errorLog: errorLog,
+	infoLog: infoLog,
+	snippets: &mysql.SnippetModel{DB: db},
+	templateCache: templateCache,
+	}
+	srv := &http.Server{
+	Addr: *addr,
+	ErrorLog: errorLog,
+	Handler: app.routes(),
+	}
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
 	}
 	
+	func openDB(dsn string) (*sql.DB, error) {
+		db, err := sql.Open("mysql", dsn)
+		if err != nil {
+		return nil, err
+		}
+		if err = db.Ping(); err != nil {
+		return nil, err
+		}
+		return db, nil
+		}
